655f5f9d0bd9977a50faf773981fc4f84541b7f1
gyue15 <786330297@qq.com>
2017-07-25 09:51:14 +0800

    deploy

--- EthereumTest/migrations/2_deploy_contracts.js
+++ EthereumTest/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var KvStorage = artifacts.require("./KvStorage.sol");
+var Transaction = artifacts.require('./Transaction.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(KvStorage);
+  deployer.deploy(Transaction);
 };
