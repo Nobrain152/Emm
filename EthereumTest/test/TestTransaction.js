0618c198af7197b3f5f1d699524820e93d1c6a7c
151250045 <151250045@smail.nju.edu.cn>
2017-07-25 13:18:28 +0800

    TC123_Transaction Test Finish

--- EthereumTest/test/TestTransaction.js
+++ EthereumTest/test/TestTransaction.js
@@ -5,33 +5,44 @@ var Transaction = artifacts.require('./Transaction.sol');
 
 contract('Transaction', function (accounts) {
     for (var i = 0; i < 1; i++) {
-        it('What we get should be the same as what we set', function () {
+        it("A should send coin to B correctly", function () {
+            var trans;
+            var account_A = accounts[0];
+            var account_B = accounts[1];
+
+            var origin_A;
+            var origin_B;
+            var finish_A;
+            var finish_B;
+
+            var start;
+            var end;
             var amount = 10;
-            var storage = null;
-            var start = new Date();
-            //AB初始钱
-            var origin_A = 100;
-            var origin_B = 100;
-            var final_A = 0;
-            var final_B = 0;
+
             return Transaction.deployed().then(function (instance) {
-                storage = instance;
-                storage.mint(accounts[1],origin_B,{from: accounts[1]})
-                return storage.mint(accounts[0],origin_A,{from: accounts[0]})
-            }).then(function () {
+                trans = instance;
+                return trans.getBalance.call(account_A);
+            }).then(function (balance) {
+                origin_A = balance.toNumber();
+                return trans.getBalance.call(account_B);
+            }).then(function (balance) {
+                origin_B = balance.toNumber();
                 start = new Date();
-                return storage.send(accounts[1], amount,{from: accounts[0]});
+                return trans.sendCoin(account_B, amount, {from: account_A});
             }).then(function () {
-                final_A = storage.balances.call(accounts[0],{from: accounts[0]});
-                final_B = storage.balances.call(accounts[1],{from: accounts[1]});
-                return final_A;
-            }).then(function (testData) {
-                assert.equal(testData, origin_A-amount, "We trades failed");
-            }).then(function() {
-                var end = new Date();
+                end = new Date();
+                return trans.getBalance.call(account_A);
+            }).then(function (balance) {
+                finish_A = balance.toNumber();
+                return trans.getBalance.call(account_B);
+            }).then(function (balance) {
+                finish_B = balance.toNumber();
+                assert.equal(finish_A, origin_A - amount, "Fail transaction from the sender");
+                assert.equal(finish_B, origin_B + amount, "Fail transaction to the receiver");
                 var time = end - start;
                 console.log("   test time is: " + time);
-            })
-        })
+            });
+        });
+
     }
 });
